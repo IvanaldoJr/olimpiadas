6d22ad28658958f672379e00ad9c383c9e3fe6cc
Ivanaldo Junior <juniorchagas@gazetaweb.com>
2024-09-09 10:56:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa")
+    // Obtém o valor do campo de pesquisa
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se o campoPesquisa for uma string vazia
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p> ";
+        return 
+    }
+
+    // Inicializa uma string vaxia para armazenar os resultados
+    let resultados = "";
+    let titulo     = "";
+    let descricao  = "";
+    let tags       = "";
+    
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLocaleLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Cria um novo elemente HTML para cada resultado
+            resultados += `
+                    <div class="item-resultado">
+                        <h2>
+                            <a href="#" target="_blank">${dado.titulo}</a>
+                        </h2>
+                        <p class="descricao-meta">${dado.descricao}</p>
+                        <a href="${dado.link}" target="_blank">Mais informações</a>
+                    </div>  
+            `;
+        }
+     
+  
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado </p>";
+    }
+
+
+    // Atribuindo os resultados gerados a seção HTML
+    section.innerHTML = resultados
+}
+
+
---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal é uma skatista brasileira, conhecida como 'Fadinha', que se tornou a mais jovem medalhista olímpica da história do Brasil.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "skate fada sk8"
+    
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que conquistou a primeira medalha de ouro para o Brasil nos Jogos Olímpicos de Paris 2024.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", // Substitua pelo link correto da Wikipédia,
+      tags: "judô judo judoka"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas olímpicas, incluindo o ouro no individual geral nos Jogos Olímpicos de Paris 2024.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginástica ginastica ouro"
+    }
+  ];
+
+   
